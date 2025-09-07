98e64240ed33fd846e68365a1621ec2401fb2d66
mahatav6 <mahatabcall56@gmail.com>
2025-09-07 22:05:43 +0600

    tailwind animate added

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="">Spondon care</h1>
-      <Button>Click me</Button>
+      <h1 className="text-2xl font-extrabold">Spondon care</h1>
+      <p className=" text-2xl font-bold">hello</p>
+      <Button className={"font-bold"}>Click me</Button>
     </div>
   );
 }
