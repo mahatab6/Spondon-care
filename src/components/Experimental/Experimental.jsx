9f75cf71a205124d893bf18e9a7f160e90d9fa15
moktadir-mirul <moktadir.mirul@gmail.com>
2025-09-09 20:10:59 +0600

    for experimental purpose only

---
+++ src/components/Experimental/Experimental.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const Experimental = () => {
+    return (
+        <div>
+            Hello! I am Mirul.
+        </div>
+    );
+};
+
+export default Experimental;
