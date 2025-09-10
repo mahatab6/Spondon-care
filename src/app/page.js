b1df77b93e9a18406b754369dc432ed482923b8a
moktadir-mirul <moktadir.mirul@gmail.com>
2025-09-10 09:35:47 +0600

    playfair display font added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from "@/components/Home/Banner";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Banner></Banner>
       <h1 className="text-2xl font-extrabold">Spondon care</h1>
       <p className=" text-2xl font-bold">hello</p>
       <Button variant={"outline"} className={"font-bold"}>Click me</Button>
---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1 className='font-saira text-5xl'>I am banner</h1>
+        </div>
+    );
+};
+
+export default Banner;
