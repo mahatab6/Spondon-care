56f429c03ae1ddb391cc3e2502e273c183d1a183
mahatav6 <mahatabcall56@gmail.com>
2025-09-10 16:42:51 +0600

    services section work done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Services from "@/components/Home/services";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Services/>
       <h1 className="text-2xl font-extrabold">Spondon care</h1>
       <p className=" text-2xl font-bold">hello</p>
       <Button variant={"outline"} className={"font-bold"}>Click me</Button>
