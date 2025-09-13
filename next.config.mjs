96cfcdc5aec91d5065ce379382656165c40499b5
mahatav6 <mahatabcall56@gmail.com>
2025-09-13 22:44:49 +0600

    test page added

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.ibb.co.com',
       },
+      {
+      protocol: "https",
+      hostname: "i.ibb.co",
+      pathname: "/**",
+    },
     ],
   },
 };
---
+++ src/app/test/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TestPage from './components/testPage'
+
+export default function page() {
+  return (
+    <div>
+        <TestPage/>
+    </div>
+  )
+}
